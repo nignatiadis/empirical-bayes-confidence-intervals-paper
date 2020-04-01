8db57c36f69af7cdc043c33564560c90460f72e5
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2020-04-01 02:58:10 -0700

    add basic code to evaluate simulations

---
+++ src/mceb_sim_eval.jl
@@ -0,0 +1,33 @@
+function mceb_sim_eval(i; eb_prior = eb_prior,
+	                n = 10_000,
+                  alpha_level = 0.9,
+                  eb_methods = eb_methods,
+                  targets = targets)
+   res_tuples = []
+  
+   μs = rand(eb_prior, n) #
+   Zs = StandardNormalSample.(μs .+ randn(n))
+  
+    for method in eb_methods
+        method_setup = fit(method, Zs)
+        for t in targets
+            true_target = t(eb_prior)
+            method_fit = fit(method_setup, t) 
+            result_nt = MCEB.target_bias_std(t, method_fit)
+            ci_nt = confint(t, method_fit; level=alpha_level)
+       
+            res_nt = (target = t, 
+                      true_dist = eb_prior,
+											n = n,
+                      iteration = i,
+                      method = method,
+                      target_value = true_target, 
+                      result_nt..., 
+                      lower_ci = ci_nt[1],
+                      upper_ci = ci_nt[2])
+                
+            push!(res_tuples, res_nt)
+        end
+    end
+	res_tuples
+end
