d79535e6509247fe659920ed47000f2ed45b3134
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2020-04-03 13:26:31 -0700

    update to plotting code

--- literate/simulation_plots.jl
+++ literate/simulation_plots.jl
@@ -8,72 +8,136 @@ using ExponentialFamilies
 using MosekTools
 using LaTeXStrings
 using FileIO
-
+using EBayes
 
 
 const MCEB = MinimaxCalibratedEBayes
 
 include(srcdir("plotting_helpers.jl"))
 
-res = load(datadir("sims","twintowers.jld2"), "res")
-res_tuples = vcat(res...)
+twintowers = load(datadir("sims","twintowers.jld2"), "res")
+bimodal = load(datadir("sims","bimodal.jld2"), "res")
+bimodal = filter( x-> x != "error", bimodal)
 
-method_string(::MinimaxCalibratedEBayes)
-	
-method_string(::ExponentialFamilyDeconvolutionMLE) = "G-model"
-method_string(::MinimaxCalibratorOptions) = "MCEB"
+unimodal = load(datadir("sims","unimodal_postmean.jld2"), "res")
+varying_dof = load(datadir("sims","varyingdof_lfsr.jld2"), "res")
 
-method_params(cefm_mle::ExponentialFamilyDeconvolutionMLE) = cefm_mle.cefm.Q_dim
-method_params(cefm_mle) = NaN
+res_tuples = vcat([twintowers; bimodal; unimodal; varying_dof]...)
 
 res_tuples_process = @from i in res_tuples begin
        @select  {i..., realized_error = i.estimated_target - i.target_value,
                        covers = i.lower_ci <= i.target_value <= i.upper_ci,
 											 ci_width = i.upper_ci - i.lower_ci,					 
-                       location = response(i.target),
-					   prior_name = string(typeof(i.true_dist))[1:10],
+					   prior_name = prior_string(i.true_dist),
 					   method_name = method_string(i.method),
 					   method_params = method_params(i.method)}
        @collect Table
 end
 
 
-
 res_df = @from i in res_tuples_process begin
-       @group i by {i.target, i.prior_name, i.method_name, i.target_value} into g
-       @select {key(g)...,
-                estimated_target = mean(g.estimated_target),
-                coverage = mean(g.covers),
-								realized_bias = mean(g.realized_error),
-								realized_std = std(g.estimated_target),
-								estimated_bias  = mean(g.estimated_bias),
-								estimated_std  = mean(g.estimated_std),
-								ci_width = mean(g.ci_width),
-								lower_ci = mean(g.lower_ci),
-								upper_ci = mean(g.upper_ci),
-								true_dist = g.true_dist[1],
-								target_string = MCEB.pretty_label(key(g).target),
-								target_location = response(key(g).target)}
-       @collect Table
+         @group i by {i.target, i.prior_name, i.method_name, i.method_params, i.target_value} into g
+         @select    {key(g)...,
+	                estimated_target = mean(g.estimated_target),
+	                coverage = mean(g.covers),
+					realized_bias = mean(g.realized_error),
+					realized_std = std(g.estimated_target),
+					estimated_bias  = mean(g.estimated_bias),
+					estimated_std  = mean(g.estimated_std),
+					ci_width = mean(g.ci_width),
+					lower_ci = mean(g.lower_ci),
+					upper_ci = mean(g.upper_ci),
+					true_dist = g.true_dist[1],
+					target_string = MCEB.pretty_label(key(g).target),
+					target_type = target_string(key(g).target),
+					target_location = response(key(g).target)}
+         @collect Table
 end
 
 
+bimodal_postmean = res_df |> @filter(_.target_type == "Posterior Mean" &&
+                                     _.prior_name == "Bimodal") |> Table
+
+twintower_postmean = res_df |> @filter(_.target_type == "Posterior Mean" &&
+                                       _.prior_name == "Smoothed Twin-Towers") |> Table
+
+
+bimodal_lfsr = res_df |> @filter(_.target_type == "LFSR" &&
+								 _.prior_name == "Bimodal") |> Table
+
+twintower_lfsr = res_df |> @filter(_.target_type == "LFSR" &&
+								   _.prior_name == "Smoothed Twin-Towers") |> Table
+
+
+unimodal_lfsr = res_df |> @filter(_.target_type == "LFSR" && 
+                                  _.prior_name == "Unimodal" &&
+								  (_.method_name == "MCEB" || _.method_params == 5))|> Table 
+								  #
+unimodal_postmean = res_df |> @filter(_.target_type == "Posterior Mean" &&
+								     _.prior_name == "Unimodal") |> Table
+
+      
+
+def_size = (800,240)
+
+pl1 = simulationplot(unimodal_lfsr; size=def_size)
+
+pl_unimodal_postmean = simulationplot(unimodal_postmean; size=def_size)
+
+savefig("pl_unimodal_postmean.svg")
+savefig("pl_unimodal_postmean.pdf")
 
 
 
+pl1 = simulationplot(bimodal_postmean; size=def_size)
 
+pl1 = simulationplot(bimodal_lfsr; size=def_size)
 
+pl1 = simulationplot(twintower_postmean; density_xlim = (-4,6), size=def_size)
 
-
-
-
-
-
-
-gr()
-pl1 = simulationplot(mydf_lfsr; density_xlim = (-4,6),
-                      size=(800,300))
+pl1 = simulationplot(twintower_lfsr; density_xlim = (-4,6), def_size)
 					  
 savefig(pl1, "pl1.svg")					  
 savefig(pl1, "pl1.pdf")					  
+
+
+
+# # Figure 2
+
+gmodel_lfsr = res_df |> @filter(_.target_type == "LFSR" && 
+                                _.prior_name == "Unimodal") |> Table
+
+gmodel_lfsr_collapse = @from i in gmodel_lfsr begin
+	                   @group i by {i.target_type, i.prior_name, i.method_name, i.method_params} into g
+	                   @select    {key(g)...,
+			                       coverage = mean(g.coverage),
+			                       ci_width = mean(g.ci_width)}
+					   @collect Table end 
+
+
+gmodel_lfsr_collapse_gmodel = gmodel_lfsr_collapse |> @filter(_.method_name == "G-model")|> Table
+gmodel_lfsr_collapse_mceb = gmodel_lfsr_collapse |> @filter(_.method_name == "MCEB")|> Table
+plot(gmodel_lfsr_collapse_gmodel.method_params, gmodel_lfsr_collapse_gmodel.coverage, seriestype=:scatter)
+
+
+pgfplotsx()
+varying_dof_plot = plot(gmodel_lfsr_collapse_mceb.ci_width, gmodel_lfsr_collapse_mceb.coverage,
+                        seriestype=:scatter, 
+                        marker=:utriangle, markersize=20, label="MCEB", color="#CD5496", markerstrokewidth=1.3)
+varying_dof_plot = plot!(varying_dof_plot, 
+                        gmodel_lfsr_collapse_gmodel.ci_width, gmodel_lfsr_collapse_gmodel.coverage,
+                        seriestype=:scatter,
+			            markersize=20, label="G-model", legend=:bottomright, color=:darkblue,
+			            bg_legend=:transparent, fg_legend=:transparent, alpha = range(0.2, 0.5, length=14),
+						markerstrokealpha = 1, markerstrokecolor =:black,
+			            ylim=(-0.06,1.1), size=(550,300), markerstrokewidth=1.3,
+						xlabel="Average CI width", ylabel="Coverage")
+annotate!(gmodel_lfsr_collapse_gmodel.ci_width, 
+          gmodel_lfsr_collapse_gmodel.coverage, 
+		  string.(gmodel_lfsr_collapse_gmodel.method_params), color=:black)
+
+hline!(varying_dof_plot, [0.9],  color = :grey, linestyle = :dash, label="")
+
+savefig("gmodel_varying_dof.svg")
+savefig("gmodel_varying_dof2.pdf")
 
--- src/plotting_helpers.jl
+++ src/plotting_helpers.jl
@@ -1,21 +1,64 @@
 using StatsPlots
 using RecipesBase
+using Colors
+
+
+
+
+function prior_string(dist::MixtureModel)
+	_comps = components(dist)
+	_probs = probs(dist)
+	iwbimod = EBayes.IWBimod().distribution
+	iwunimod = EBayes.IWUnimod().distribution
+	if _probs == probs(iwbimod) && _comps == components(iwbimod)
+		return("Bimodal")
+	elseif _probs == probs(iwunimod) && _comps == components(iwunimod)
+		return("Unimodal")
+	else
+		return("Mixture")
+	end
+end
+
+prior_string(::ContinuousExponentialFamily) = "Smoothed Twin-Towers"
+prior_string(dbn) = string(typeof(dbn))[1:10]
+
+method_string(::ExponentialFamilyDeconvolutionMLE) = "G-model"
+method_string(::MinimaxCalibratorOptions) = "MCEB"
+
+method_params(cefm_mle::ExponentialFamilyDeconvolutionMLE) = cefm_mle.cefm.Q_dim
+method_params(cefm_mle) = 0
+
+target_string(::MCEB.PosteriorTarget{<:PosteriorMeanNumerator}) = "Posterior Mean"
+target_string(::MCEB.PosteriorTarget{<:LFSRNumerator}) = "LFSR"
+
+
+
+
+
 
 @userplot SimulationPlot
+# "#550133"
 
+# mceb color "#EBA415"
+# gmodel color "#550133"
 @recipe function f(h::SimulationPlot; method_names= ["MCEB";"G-model"],
 	                                  density_xlim = nothing,
-									  titles = ["a)"; "b)"; "c)"])
+									  titles = ["a)"; "b)"; "c)"],
+									  gmodel_alpha = 0.5,
+									  gmodel_color =  "#C68642",# "#C2C2B4", #"#550133", 
+									  mceb_color = "#F6CD7F") #"#EBA415")#)
     res_df = first(h.args)
 	mceb_df = res_df |> @filter(_.method_name == method_names[1]) |> Table
 	gmodel_df = res_df |> @filter(_.method_name == method_names[2]) |> Table
     
 	true_prior = res_df.true_dist[1]
+	prior_name = res_df.prior_name[1]
+
 	layout := @layout [panel1 panel2 panel3]
 
 	fg_legend --> :transparent
 	bg_legend --> :transparent
-	xlim --> extrema(res_df.target_location)
+	xlim --> extrema(res_df.target_location) .+ (-0.043,0.01)
 	
     # Plot density
     @series begin
@@ -26,27 +69,33 @@ using RecipesBase
 			xlim := density_xlim
 		end 
         #xlim := extrema(x_grid)
+		label := prior_name
+		legend := :topright
 		color --> "#018AC4"
 		xguide := L"\mu"
-		yguide := L"g(\mu)"
+		yguide := L"Density $g(\mu)$"
+		if isa(true_prior, MixtureModel)
+			components := false
+		end
         true_prior
     end
 
 	@series begin
-		subplot := 2
-		color := "#EBA415"
-		fillalpha := 1.0
-		ribbon --> (mceb_df.estimated_target .- mceb_df.lower_ci, mceb_df.upper_ci .- mceb_df.estimated_target)
-		mceb_df.target_location, mceb_df.estimated_target
+	 	subplot := 2
+		fillrange := mceb_df.upper_ci
+		color := mceb_color
+		label := method_names[1]*" Band"
+		mceb_df.target_location, mceb_df.lower_ci
 	end
 	
 	@series begin
 		subplot := 2
-		color := "#550133"
-		alpha := 0.4
-		ribbon --> (gmodel_df.estimated_target .- gmodel_df.lower_ci, gmodel_df.upper_ci .- gmodel_df.estimated_target)
-		gmodel_df.target_location, gmodel_df.estimated_target
+		fillrange := gmodel_df.upper_ci
+		color := gmodel_color
+		label := method_names[2]*" Band"
+		gmodel_df.target_location, gmodel_df.lower_ci
 	end
+
 	
 	@series begin
 		seriestype := :path
@@ -56,6 +105,7 @@ using RecipesBase
 		label := "true target"
 		ylabel --> MCEB.pretty_label(res_df.target[1])
 		linestyle := :dot
+		legend --> :bottomright
 		color := :black
 		mceb_df.target_location, mceb_df.target_value
 	end
@@ -64,7 +114,7 @@ using RecipesBase
 		subplot :=3
 		seriestype := :path
 		label --> method_names[1]
-		color --> "#EBA415"#"#EBC915"
+		color --> mceb_color     #"#EBA415"#"#EBC915"
 		alpha --> 1
 		linewidth --> 1.5
 		mceb_df.target_location, mceb_df.coverage
@@ -83,8 +133,8 @@ using RecipesBase
 		seriestype := :path
 		xguide := L"x"
 		ylim --> (0,1.3)
-		color --> "#550133"
-		alpha -> 0.4
+		color --> gmodel_color#"#550133"
+		#alpha -> 0.4
 		linewidth --> 1.5
 		yguide --> "Coverage"
 		legend --> :topright
