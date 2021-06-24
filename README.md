06f37092672bf381b4278e9950a0e8f606ae14f0
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2021-06-24 07:16:30 -0700

    start manifest for v4, add asymptotic risk plots

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A repository for reproducing the numerical results in the following preprint:
 
 See [Empirikos.jl](https://github.com/nignatiadis/Empirikos.jl) for the Julia package implementing the proposed method.
 
-All computations were run on [Julia](https://julialang.org/) version 1.5.1 and [Mosek](https://www.mosek.com/) version 9.2. See `Manifest.toml` for specifications of all Julia packages used (in particular note that the `Empirikos.jl` interface has changed in its latest release). 
+All computations were run on [Julia](https://julialang.org/) version 1.6.1 and [Mosek](https://www.mosek.com/) version 9.2. See `Manifest.toml` for specifications of all Julia packages used.
 
 ## File description
 
@@ -16,7 +16,9 @@ All computations were run on [Julia](https://julialang.org/) version 1.5.1 and [
   * `simulation_ebci.jl`: Main script for running simulations. It can be called for example as `julia -t 10 simulation_ebci.jl postmean negspiky` where 10 is the number of threads. See the `_simulation_*.sbatch` files for all the calls of this script reported in the manuscript.
   * `simulation_expfamily.jl`: Script to run simulations with Logspline and varying degrees of freedom.
   * `simulation_plots.jl`: Takes the output of the simulation scripts above and generates Figures 3-7 of the paper .
-
+* **Other:**
+  * `asymptotic_ci_length.jl`
 ## Repository history
 
-Version 2 of this manuscript on arXiv ([arXiv:1902.02774v2](https://arxiv.org/abs/1902.02774v2)) was substantially different. See this repository at the [arXiv_V2 release tag](https://github.com/nignatiadis/empirical-bayes-confidence-intervals-paper/releases/tag/arXiv_v2) to reproduce the results of that version.
+* Version 2 of this manuscript on arXiv ([arXiv:1902.02774v2](https://arxiv.org/abs/1902.02774v2)) was substantially different. See this repository at the [arXiv_V2 release tag](https://github.com/nignatiadis/empirical-bayes-confidence-intervals-paper/releases/tag/arXiv_v2) to reproduce the results of that version.
+* Similarly, you can find the code for version 3  ([arXiv:1902.02774v3](https://arxiv.org/abs/1902.02774v3)) at the [[arXiv_V3 release tag](https://github.com/nignatiadis/empirical-bayes-confidence-intervals-paper/releases/tag/arXiv_v3) ].
---
+++ asymptotic_ci_length.jl
@@ -0,0 +1,157 @@
+using Empirikos
+using Random
+using Plots
+using MosekTools
+using LaTeXStrings
+using JuMP
+
+pgfplotsx()
+
+
+discr = integer_discretizer(0:6)
+
+K = 3
+density_target = MarginalDensity(PoissonSample(K))
+
+postmean_target = PosteriorMean(PoissonSample(K))
+
+true_G = Uniform(0,2)
+
+𝒢 = DiscretePriorClass(0:0.01:4)
+
+f2 = 1/2 - 5/(2*exp(2))
+f3 = (1-19/(3*exp(2)))/2
+f4 = 1/2 - 7/(2*exp(2))
+
+amari_dkw = AMARI(
+    flocalization = Empirikos.DvoretzkyKieferWolfowitz(0.01),
+    solver=Mosek.Optimizer, convexclass=𝒢, discretizer=discr,
+    modulus_model=Empirikos.ModulusModelWithF)
+
+floc_method = FLocalizationInterval(flocalization= Empirikos.DvoretzkyKieferWolfowitz(0.05),
+    solver=Mosek.Optimizer,
+    convexclass=𝒢)
+
+
+
+function evaluate_at_n(true_G, amari, floc, target, n; ci_only=false)
+    μs = rand(true_G, n)
+    Zs = PoissonSample.(rand.(Poisson.(μs)))
+    if ci_only
+        floc_fit = nothing
+        amari_fit = nothing
+        floc_ci = confint(floc, target, discr.(Zs))
+        amari_ci = confint(amari, target, Zs)
+
+    else
+        floc_fit = fit(floc, target, discr.(Zs))
+        floc_ci = confint(floc_fit)
+        amari_fit = fit(amari, target, Zs)
+        amari_ci = confint(amari_fit, target, Zs)
+    end
+    (n=n, amari_fit=amari_fit, amari_ci=amari_ci, floc_fit=floc_fit, floc_ci=floc_ci)
+end
+
+
+Random.seed!(1)
+
+ns = floor.(Int64, 10 .^ (2:0.5:8))
+results_density = Vector{Any}(undef, length(ns))
+
+for (i,n) in enumerate(ns)
+    results_density[i] = evaluate_at_n(true_G, amari_dkw,
+                        floc_method, density_target, n; ci_only=false)
+end
+
+
+α = 0.05
+amari_ci_lengths = [res.amari_ci.upper - res.amari_ci.lower for res in results_density]
+dkw_ci_lengths = [res.floc_ci.upper - res.floc_ci.lower for res in results_density]
+theory_ci_lengths = 2*quantile(Normal(), 1-α/2)*sqrt.(f3*(1-f3)./ns)
+theory_dkw_ci_lengths = 2*2*sqrt.(log(2/α) /2 ./ ns)
+
+
+theme(
+    :default;
+    background_color_legend = :transparent,
+    foreground_color_legend = :transparent,
+    grid = nothing,
+    frame = :box,
+    thickness_scaling = 1.3,
+    legendfonthalign = :left,
+    size = (420, 330),
+)
+
+
+poisson_density_relative_efficiency =
+    plot(ns, [amari_ci_lengths theory_ci_lengths dkw_ci_lengths theory_dkw_ci_lengths],
+       scale=:log10,
+       label=[L"\textrm{AMARI}" L"\textrm{AMARI asymp.}" L"\textrm{DKW-F-Loc}" L"\textrm{DKW-F-Loc asymp.}"],
+       legend= :topright,
+       color=[:blue :blue :darkorange :darkorange],
+       linestyle=[:solid :dot :solid :dot],
+       xguide = L"n",
+       yguide = "CI length",
+       size=(420,300)
+       )
+
+
+savefig(poisson_density_relative_efficiency, "poisson_density_relative_efficiency.tikz")
+
+
+zs = 0:7
+Qs_3 = results_density[3].amari_fit.Q.(PoissonSample.(zs))
+Qs_7 = results_density[7].amari_fit.Q.(PoissonSample.(zs))
+Qs_11 = results_density[11].amari_fit.Q.(PoissonSample.(zs))
+
+_strings = ["Q($z)" for z in zs]
+
+Q_plot = plot(zs .- 0.15, Qs_3, seriestype=:sticks, label = L"n=10^3", color=:lightgrey,
+    legend=:topright, linewidth=1.3, yticks=-0.5:0.25:1.0,
+    ylims=(-0.55,1.05), xticks=(0:7,_strings), size=(420,300),
+    xguide=L"\phantom{-}"
+    )
+
+plot!(Q_plot, zs, Qs_7, seriestype=:sticks,  label = L"n=10^5",
+     linewidth=1.3, color=:lightblue)
+plot!(Q_plot, zs .+ 0.15, Qs_11, seriestype=:sticks,  label = L"n=10^9",
+     linewidth=1.3, color=:darkblue, linealpha=0.4)
+
+savefig(Q_plot, "Q_plot.tikz")
+
+
+ns = floor.(Int64, 10 .^ (2:0.5:8))
+
+results_postmean = Vector{Any}(undef, length(ns))
+
+Random.seed!(1)
+
+for (i,n) in enumerate(ns)
+    results_postmean[i] = evaluate_at_n(true_G, amari_dkw,
+                        floc_method, postmean_target, n; ci_only=true)
+end
+
+
+α = 0.05
+postmean_amari_ci_lengths = [res.amari_ci.upper - res.amari_ci.lower for res in results_postmean]
+postmean_dkw_ci_lengths = [res.floc_ci.upper - res.floc_ci.lower for res in results_postmean]
+var_delta = (K+1)^2*f4/abs2(f3)*(1 + f4/f3)
+postmean_theory_ci_lengths = 2*quantile(Normal(), 1-α/2)*sqrt(var_delta)./sqrt.(ns)
+postmean_theory_dkw_ci_lengths = (K+1)*2*2*sqrt.(log(2/α) /2 ./ ns) * (1 + f4/f3) / f3
+
+
+
+poisson_postmean_relative_efficiency =
+    plot(ns, [postmean_amari_ci_lengths postmean_theory_ci_lengths postmean_dkw_ci_lengths postmean_theory_dkw_ci_lengths],
+       scale=:log10,
+       label=[L"\textrm{AMARI}" L"\textrm{AMARI asymp.}" L"\textrm{DKW-F-Loc}" L"\textrm{DKW-F-Loc asymp.}"],
+       legend= :topright,
+       color=[:blue :blue :darkorange :darkorange],
+       linestyle=[:solid :dot :solid :dot],
+       xguide = L"n",
+       yguide = "CI length",
+       size=(420,300)
+       )
+
+
+savefig(poisson_postmean_relative_efficiency, "poisson_postmean_relative_efficiency.tikz")
